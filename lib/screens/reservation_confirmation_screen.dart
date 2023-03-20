8ba29d0afd6143d26103623f09bc205f394cd133
otayib <97808995+otayib@users.noreply.github.com>
2023-03-21 02:53:08 +0300

    Update reservation_confirmation_screen.dart
    more styling for Ui  ReservationConfirmationScreen

--- lib/screens/reservation_confirmation_screen.dart
+++ lib/screens/reservation_confirmation_screen.dart
@@ -10,22 +10,47 @@ class ReservationConfirmationScreen extends StatelessWidget {
   static String routeName = '/confirmation-screen';
   late List receivedArgs;
 
+  void createReservation(
+      DateTime date, String peroid, VenueItem venue, Map list) {
+    Reservation reservation = Reservation(
+        reservationNumber: 100 + Random().nextInt(900),
+        reservationDate: date,
+        reservationStatus: 'pending',
+        reservationTime: peroid,
+        reservedVenue: venue,
+        listOfAttendants: list);
 
+    updateList(reservation);
+  }
 
-  void createReservation(DateTime date, String peroid, VenueItem venue, Map list){
-     Reservation reservation = Reservation(
-         reservationNumber: 100 + Random().nextInt(900),
-         reservationDate: date,
-         reservationStatus: 'pending',
-         reservationTime: peroid,
-         reservedVenue: venue,
-         listOfAttendants: list);
+  //orginal style
+  // @override
+//   Widget build(BuildContext context) {
+//     receivedArgs = ModalRoute.of(context)?.settings.arguments as List;
+//     VenueItem venue = (receivedArgs[0] as List)[0];
+//     DateTime dt = (receivedArgs[0] as List)[1];
+//     String chosenPeriod = (receivedArgs[0] as List)[2];
+//     Map m = receivedArgs[1];
+//     return Scaffold(appBar: AppBar(title: Text('confirm'),),
+//       body: Column(children: [
+//         Text(venue.title),
+//         Text(venue.capacity.toString()),
+//         Text(dt.toString()),
+//         Text(chosenPeriod),
+//         Text(m.toString()),
+//         ElevatedButton(onPressed: (){
+//           createReservation(dt,chosenPeriod,venue, m);
+//           //TODO MAKE BOOKING SUCCESS PAGE ISNTEAD OF DIRECTLY GOING HOME
+//           Navigator.of(context).pushNamed(ReservationSuccessfulScreen.routeName);
+//         }, child: Text('submit reservation request'),)
 
-     updateList(reservation);
+//       ],)
+//       ,);
+//   }
+// }
 
-
-   }
-
+//new style
+// new style
   @override
   Widget build(BuildContext context) {
     receivedArgs = ModalRoute.of(context)?.settings.arguments as List;
@@ -33,20 +58,70 @@ class ReservationConfirmationScreen extends StatelessWidget {
     DateTime dt = (receivedArgs[0] as List)[1];
     String chosenPeriod = (receivedArgs[0] as List)[2];
     Map m = receivedArgs[1];
-    return Scaffold(appBar: AppBar(title: Text('confirm'),),
-      body: Column(children: [
-        Text(venue.title),
-        Text(venue.capacity.toString()),
-        Text(dt.toString()),
-        Text(chosenPeriod),
-        Text(m.toString()),
-        ElevatedButton(onPressed: (){
-          createReservation(dt,chosenPeriod,venue, m);
-          //TODO MAKE BOOKING SUCCESS PAGE ISNTEAD OF DIRECTLY GOING HOME
-          Navigator.of(context).pushNamed(ReservationSuccessfulScreen.routeName);
-        }, child: Text('submit reservation request'),)
-
-      ],)
-      ,);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Confirm Reservation'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              venue.title,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Capacity: ${venue.capacity}',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Reservation Date and Time: ${dt.toString()}',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Chosen Period: $chosenPeriod',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'players Information: ',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              m.toString(),
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () {
+                createReservation(dt, chosenPeriod, venue, m);
+                Navigator.of(context)
+                    .pushNamed(ReservationSuccessfulScreen.routeName);
+              },
+              child: Text('Submit Reservation Request'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
