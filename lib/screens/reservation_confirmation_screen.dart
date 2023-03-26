57af96b684d00b53dcd124c51e07faf08087d424
otayib <97808995+otayib@users.noreply.github.com>
2023-03-26 04:15:01 +0300

    Update reservation_confirmation_screen.dart
    add more function to the screens with list and list view builder

--- lib/screens/reservation_confirmation_screen.dart
+++ lib/screens/reservation_confirmation_screen.dart
@@ -14,15 +14,14 @@ import '../widgets/period_buttons.dart';
 class ReservationConfirmationScreen extends StatelessWidget {
   static String routeName = '/confirmation-screen';
   late List receivedArgs;
-  
-  String formatDate(DateTime dt){
+
+  String formatDate(DateTime dt) {
     var format = DateFormat('yyyy-MM-dd');
     return format.format(dt);
   }
 
   Future createReservation(
-      DateTime date, String peroid, VenueItem venue, Map list) async{
-
+      DateTime date, String peroid, VenueItem venue, Map list) async {
     Reservation reservation = Reservation(
         reservationNumber: 100 + Random().nextInt(900),
         formattedDate: formatDate(date),
@@ -35,46 +34,49 @@ class ReservationConfirmationScreen extends StatelessWidget {
 
     print('object is done');
     print(reservation);
-    final docUser =
-    FirebaseFirestore.instance.collection('reservations').doc();
-    await docUser.set(Reservation.toJson(
-        reservation,
-        FirebaseAuth.instance.currentUser!.uid,
-        venue.typeOfSport
-    ));
+    final docUser = FirebaseFirestore.instance.collection('reservations').doc();
+    await docUser.set(Reservation.toJson(reservation,
+        FirebaseAuth.instance.currentUser!.uid, venue.typeOfSport));
     print('sent the json done');
 
     //updateList(reservation);
   }
 
   //orginal style
-  // @override
+//   @override
 //   Widget build(BuildContext context) {
 //     receivedArgs = ModalRoute.of(context)?.settings.arguments as List;
 //     VenueItem venue = (receivedArgs[0] as List)[0];
 //     DateTime dt = (receivedArgs[0] as List)[1];
 //     String chosenPeriod = (receivedArgs[0] as List)[2];
 //     Map m = receivedArgs[1];
-//     return Scaffold(appBar: AppBar(title: Text('confirm'),),
-//       body: Column(children: [
-//         Text(venue.title),
-//         Text(venue.capacity.toString()),
-//         Text(dt.toString()),
-//         Text(chosenPeriod),
-//         Text(m.toString()),
-//         ElevatedButton(onPressed: (){
-//           createReservation(dt,chosenPeriod,venue, m);
-//           //TODO MAKE BOOKING SUCCESS PAGE ISNTEAD OF DIRECTLY GOING HOME
-//           Navigator.of(context).pushNamed(ReservationSuccessfulScreen.routeName);
-//         }, child: Text('submit reservation request'),)
-
-//       ],)
-//       ,);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('confirm'),
+//       ),
+//       body: Column(
+//         children: [
+//           Text(venue.title),
+//           Text(venue.capacity.toString()),
+//           Text(dt.toString()),
+//           Text(chosenPeriod),
+//           Text(m.toString()),
+//           ElevatedButton(
+//             onPressed: () {
+//               createReservation(dt, chosenPeriod, venue, m);
+//               //TODO MAKE BOOKING SUCCESS PAGE ISNTEAD OF DIRECTLY GOING HOME
+//               Navigator.of(context)
+//                   .pushNamed(ReservationSuccessfulScreen.routeName);
+//             },
+//             child: Text('submit reservation request'),
+//           )
+//         ],
+//       ),
+//     );
 //   }
 // }
 
-//new style
-// new style
+//style 2
   @override
   Widget build(BuildContext context) {
     receivedArgs = ModalRoute.of(context)?.settings.arguments as List;
@@ -82,66 +84,143 @@ class ReservationConfirmationScreen extends StatelessWidget {
     DateTime dt = (receivedArgs[0] as List)[1];
     String chosenPeriod = (receivedArgs[0] as List)[2];
     Map m = receivedArgs[1];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Confirm Reservation'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              venue.title,
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    venue.imageUrl,
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          venue.title,
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Center(
+                    child: Card(
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Reservation Date and Time:',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              dt.toString(),
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Text(
+                              'Chosen Period:',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              chosenPeriod,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Players Information:',
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Container(
+                          height: 300,
+                          width: double.infinity,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: m.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              String key = m.keys.elementAt(index);
+
+                              return Container(
+                                padding: EdgeInsets.symmetric(vertical: 8.0),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        key,
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        m[key].toString(),
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 32.0),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        createReservation(dt, chosenPeriod, venue, m);
+                        Navigator.of(context)
+                            .pushNamed(ReservationSuccessfulScreen.routeName);
+                      },
+                      child: Text('Submit Reservation Request'),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Capacity: ${venue.capacity}',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              'Reservation Date and Time: ${dt.toString()}',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              'Chosen Period: $chosenPeriod',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'players Information: ',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              m.toString(),
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                createReservation(dt, chosenPeriod, venue, m);
-                Navigator.of(context)
-                    .pushNamed(ReservationSuccessfulScreen.routeName);
-              },
-              child: Text('Submit Reservation Request'),
             ),
           ],
         ),
@@ -149,3 +228,152 @@ class ReservationConfirmationScreen extends StatelessWidget {
     );
   }
 }
+
+  //  style3
+//   @override
+//   Widget build(BuildContext context) {
+//     receivedArgs = ModalRoute.of(context)?.settings.arguments as List;
+//     VenueItem venue = (receivedArgs[0] as List)[0];
+//     DateTime dt = (receivedArgs[0] as List)[1];
+//     String chosenPeriod = (receivedArgs[0] as List)[2];
+//     Map m = receivedArgs[1];
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Confirm Reservation'),
+//       ),
+//       body: SingleChildScrollView(
+//         scrollDirection: Axis.vertical,
+//         child: Column(
+//           children: [
+//             Container(
+//               padding: EdgeInsets.all(16.0),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Image.network(
+//                     venue.imageUrl,
+//                     width: double.infinity,
+//                     height: 200,
+//                     fit: BoxFit.cover,
+//                   ),
+//                   Center(
+//                     child: Column(
+//                       children: [
+//                         Text(
+//                           venue.title,
+//                           style: TextStyle(
+//                             fontSize: 24.0,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                   SizedBox(height: 16.0),
+//                   Center(
+//                     child: Card(
+//                       child: Container(
+//                         padding: EdgeInsets.all(16.0),
+//                         child: Column(
+//                           crossAxisAlignment: CrossAxisAlignment.start,
+//                           children: [
+//                             Text(
+//                               'Reservation Date and Time:',
+//                               style: TextStyle(
+//                                 fontSize: 16.0,
+//                                 fontWeight: FontWeight.bold,
+//                               ),
+//                             ),
+//                             Text(
+//                               dt.toString(),
+//                               style: TextStyle(
+//                                 fontSize: 16.0,
+//                               ),
+//                             ),
+//                             SizedBox(height: 8.0),
+//                             Text(
+//                               'Chosen Period:',
+//                               style: TextStyle(
+//                                 fontSize: 16.0,
+//                                 fontWeight: FontWeight.bold,
+//                               ),
+//                             ),
+//                             Text(
+//                               chosenPeriod,
+//                               style: TextStyle(
+//                                 fontSize: 16.0,
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   SizedBox(height: 16.0),
+//                   Center(
+//                     child: Column(
+//                       children: [
+//                         Text(
+//                           'Players Information:',
+//                           style: TextStyle(
+//                             fontSize: 18.0,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         SizedBox(height: 8.0),
+//                         Container(
+//                           height: 300,
+//                           child: ListView(
+//                             children: m.keys.map((key) {
+//                               return Container(
+//                                 padding: EdgeInsets.symmetric(vertical: 8.0),
+//                                 child: Row(
+//                                   children: [
+//                                     Expanded(
+//                                       child: Text(
+//                                         key,
+//                                         style: TextStyle(
+//                                           fontSize: 16.0,
+//                                           fontWeight: FontWeight.bold,
+//                                         ),
+//                                       ),
+//                                     ),
+//                                     SizedBox(width: 8.0),
+//                                     Expanded(
+//                                       child: Text(
+//                                         m[key].toString(),
+//                                         style: TextStyle(
+//                                           fontSize: 16.0,
+//                                         ),
+//                                       ),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               );
+//                             }).toList(),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                   SizedBox(height: 32.0),
+//                   Center(
+//                     child: ElevatedButton(
+//                       onPressed: () {
+//                         createReservation(dt, chosenPeriod, venue, m);
+//                         Navigator.of(context)
+//                             .pushNamed(ReservationSuccessfulScreen.routeName);
+//                       },
+//                       child: Text('Submit Reservation Request'),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
